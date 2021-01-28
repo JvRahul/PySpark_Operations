6fb3ceef9e24a45881848fd32763b8bd3a00cbac
Venkata Rahul Jammalamadaka <35979159+JvRahul@users.noreply.github.com>
2021-01-28 11:55:28 -0500

    Update json_read_df.py

--- json_read_df.py
+++ json_read_df.py
@@ -1,11 +1,12 @@
 df = spark.read.json('/user/path.json')
 df.show()  #it works only on a single line json otherwise it would throw a corrupt_record error
 
-To handle multiple lines json file,
+#To handle multiple lines json file,
 
-use df = spark.read.json('/user/path.json', multiLine = True)
+#use 
+df = spark.read.json('/user/path.json', multiLine = True)
 
-If we got a column (colname) of array type in the dataframe, split it using explode function as below,
+#If we got a column (colname) of array type in the dataframe, split it using explode function as below,
 
 from pyspark.sql.functions import explode
 df_flat = df.withColumn('colname_flat', explode('colname'))
